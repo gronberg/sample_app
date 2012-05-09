0d5955d138a25075dd8cffb0feb8f500e3457130
gronberg <john.gronberg@tapjoy.com>
2012-05-09 16:21:12 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2b61e11544a88e34c023af5f2f8a11c8162a5181a1bfbf389689a5ce59cf8410b85cb94fae89e1442cefaf6c6daeecde60e7f087e92214c7c3e19d9fc485d33f'
